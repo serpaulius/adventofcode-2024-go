6133085f705404ec54c4d1f9921dd3edb4f3e54b
serpaulius <7099502+serpaulius@users.noreply.github.com>
2024-12-04 13:22:20 +0200

    fourth day pt2

--- 4/fourth.go
+++ 4/fourth.go
@@ -7,23 +7,18 @@ import (
 	"strings"
 )
 
-func transpose(lines []string) []string {
-	transposed := make([]string, len(lines[0]))
-	for _, line := range lines {
-		for j, strChar := range line {
-			transposed[j] = transposed[j] + string(strChar)
-		}
-	}
-	fmt.Println(transposed)
-	return transposed
-}
-
-type Vector struct {
+type Coordinate struct {
 	x int
 	y int
 }
 
-var vectors []Vector = []Vector{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}}
+func (v1 Coordinate) add(v2 Coordinate) Coordinate {
+	return Coordinate{v1.x + v2.x, v1.y + v2.y}
+}
+
+// x goes right, y goes bottom, listing vectors to corners from top-left clockwise:
+var cornerVectors = []Coordinate{{-1, -1}, {1, -1}, {1, 1}, {-1, 1}}
+var vectors []Coordinate = append([]Coordinate{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}, cornerVectors...)
 
 type Grid struct {
 	lines         []string
@@ -37,37 +32,96 @@ func NewGrid(lines []string) *Grid {
 	return grid
 }
 
-func (g Grid) getLetter(cx, cy int) string {
-	return string(g.lines[cy][cx])
+func (g Grid) getLetterByCoordinate(c Coordinate) string {
+	return string(g.lines[c.y][c.x])
 }
 
-func (g Grid) countWordsForCoordinate(word string, x, y int) int {
+func (g Grid) findAll(letter string) []Coordinate {
+	var occurences []Coordinate
+	c := Coordinate{}
+	for c.y = range g.height {
+		for c.x = range g.width {
+			if strings.Compare(g.getLetterByCoordinate(c), letter) == 0 {
+				occurences = append(occurences, c)
+			}
+		}
+	}
+	return occurences
+}
+
+func (g Grid) countWordsForCoordinate(word string, position Coordinate) int {
 	wordLength := len(word)
 	count := 0
 	for _, vect := range vectors {
 		candidate := ""
-		cx, cy := x, y
+		c := position
 		for i := 0; i < wordLength; i++ {
-			if cx >= 0 && cy >= 0 && cx < g.width && cy < g.height {
-				candidate += g.getLetter(cx, cy)
+			if c.x >= 0 && c.y >= 0 && c.x < g.width && c.y < g.height {
+				candidate += g.getLetterByCoordinate(c)
 			}
-			cx, cy = cx+vect.x, cy+vect.y
+			c = c.add(vect)
 		}
 		if strings.Compare(candidate, word) == 0 {
-			log.Println(Vector{x, y}, vect, candidate)
+			log.Println(position, vect, candidate)
 			count++
 		}
 	}
-	log.Println("count for", Vector{x, y}, "is", count)
+	log.Println("count for", position, "is", count)
 	return count
 }
 
-func countWords(lines []string) int {
+// find all A coordinates and check diagonal vectors for M and S in the opposite
+// 4,4 6,6 4,6 6,4
+// 0.1  M.M  S.M  ...
+// ...  .A.  .S.  ...
+// 2.3  S.S  S.M  ...
+//
+// NOT these!
+// M.S  S.M
+// .A.  .A.
+// S.M  M.S
+
+func (g Grid) isEdge(c Coordinate) bool {
+	return c.x < 1 || c.y < 1 || c.x >= g.width-1 || c.y >= g.height-1
+}
+
+func ugliestCrossMASCheck(lines []string) int {
+	grid := NewGrid(lines)
+	foundLetters := grid.findAll("A")
+
+	xMasCount := 0
+	for _, letter := range foundLetters {
+		if grid.isEdge(letter) {
+			continue
+		}
+		var cornerPositions []Coordinate
+		for _, cornerVector := range cornerVectors {
+			cornerPositions = append(cornerPositions, letter.add(cornerVector))
+		}
+
+		exactlyTwo := 0
+		for i, corner := range cornerPositions {
+			oppositeIndex := (i + 2) % 4
+			if strings.Compare(grid.getLetterByCoordinate(corner), "M") == 0 {
+				if strings.Compare(grid.getLetterByCoordinate(cornerPositions[oppositeIndex]), "S") == 0 {
+					exactlyTwo += 1
+				}
+			}
+		}
+		if exactlyTwo == 2 {
+			xMasCount += 1
+		}
+	}
+
+	return xMasCount
+}
+
+func countWordsInLines(word string, lines []string) int {
 	grid := NewGrid(lines)
 	count := 0
 	for x := 0; x < grid.width; x++ {
 		for y := 0; y < grid.height; y++ {
-			count += grid.countWordsForCoordinate("XMAS", x, y)
+			count += grid.countWordsForCoordinate(word, Coordinate{x, y})
 		}
 	}
 	log.Println("Final result", count)
@@ -80,7 +134,10 @@ func Run() {
 	scanner := util.GiveMeAScannerPlz(file)
 	lines := util.ReadLines(scanner)
 
-	result := countWords(lines)
+	result := countWordsInLines("XMAS", lines)
 	fmt.Println("4.1 - XMAS count", result)
+
+	result2 := ugliestCrossMASCheck(lines)
+	fmt.Println("4.2 - X-MAS count", result2)
 
 }
