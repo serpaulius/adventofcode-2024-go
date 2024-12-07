befca94692a196ed22a489878897e7cc2e591cc2
serpaulius <7099502+serpaulius@users.noreply.github.com>
2024-12-07 16:43:58 +0200

    7.2 another operand

--- 7/seventh.go
+++ 7/seventh.go
@@ -3,14 +3,15 @@ package seventh
 import (
 	"adventofcode/2024-go/util"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 )
 
 type Equation struct {
-	result      int64
-	members     []int64
-	memberCount int
+	result       int64
+	operands     []int64
+	operandCount int
 }
 
 func parseInput(lines []string) []Equation {
@@ -18,42 +19,74 @@ func parseInput(lines []string) []Equation {
 	for _, line := range lines {
 		split := strings.Split(line, ": ")
 		result, _ := strconv.ParseInt(split[0], 10, 64)
-		membersStrs := strings.Split(split[1], " ")
-		var members = make([]int64, len(membersStrs))
-		for i, str := range membersStrs {
+		operandsStrs := strings.Split(split[1], " ")
+		var operands = make([]int64, len(operandsStrs))
+		for i, str := range operandsStrs {
 			converted, _ := strconv.ParseInt(str, 10, 64)
-			members[i] = converted
+			operands[i] = converted
 		}
-		equations = append(equations, Equation{result: result, members: members, memberCount: len(members)})
+		equations = append(equations, Equation{result: result, operands: operands, operandCount: len(operands)})
 	}
 	return equations
 }
 
-func traverseEquation(equation Equation, result int64, memberIndex int) bool {
-	if memberIndex == equation.memberCount {
+func intLength(i int64) int64 {
+	if i == 0 {
+		return 1
+	}
+	var count int64
+	for i != 0 {
+		i /= 10
+		count++
+	}
+	return count
+}
+
+func addZeros(value int64, zeros int64) int64 {
+	var i int64
+	for i = 0; i < zeros; i++ {
+		value = value * 10
+	}
+	return value
+}
+
+func concatInts(a int64, b int64) int64 {
+	return addZeros(a, intLength(b)) + b
+}
+
+var concatEnabled = false
+
+func traverseEquation(equation Equation, result int64, operandIndex int) bool {
+	if operandIndex == equation.operandCount {
 		if result == equation.result {
+			log.Println("OK", equation)
 			return true
 		} else {
 			return false
 		}
 	}
+	operand := equation.operands[operandIndex]
 
 	var multiplyResult int64
-	if memberIndex == 0 {
-		multiplyResult = equation.members[memberIndex]
+	if operandIndex == 0 {
+		multiplyResult = operand
 	} else {
-		multiplyResult = result * equation.members[memberIndex]
+		multiplyResult = result * operand
 	}
-	res1 := traverseEquation(equation, multiplyResult, memberIndex+1)
-
-	if res1 {
-		return res1
+	if traverseEquation(equation, multiplyResult, operandIndex+1) {
+		return true
 	}
 
-	addResult := result + equation.members[memberIndex]
+	addResult := result + operand
+	if traverseEquation(equation, addResult, operandIndex+1) {
+		return true
+	}
 
-	res2 := traverseEquation(equation, addResult, memberIndex+1)
-	return res2
+	if concatEnabled {
+		return traverseEquation(equation, concatInts(result, operand), operandIndex+1)
+	}
+
+	return false
 }
 
 func traverseEquations(equations []Equation) int64 {
@@ -75,5 +108,10 @@ func Run() {
 
 	equations := parseInput(lines)
 	result := traverseEquations(equations)
-	fmt.Println("7.1 - operators on the bridge", result)
+	fmt.Println("7.1 - 2 operators on the bridge", result)
+
+	// fixme: ouch
+	concatEnabled = true
+	result2 := traverseEquations(equations)
+	fmt.Println("7.2 - 3 operators on the bridge", result2)
 }
