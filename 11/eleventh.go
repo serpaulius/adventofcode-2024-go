1876635cbb3d8fef27e555e772cbe728f9720023
serpaulius <7099502+serpaulius@users.noreply.github.com>
2024-12-13 10:23:50 +0200

    11.2 it took GBs and hrs to facepalm

--- 11/eleventh.go
+++ 11/eleventh.go
@@ -25,7 +25,7 @@ func blink(stones []int64) []int64 {
 	return newArr
 }
 
-func blinkAtStones(stones []int64, blinks int) int {
+func naiveBlink(stones []int64, blinks int) int {
 	log.Println("start", stones)
 	for i := 0; i < int(blinks); i++ {
 		stones = blink(stones)
@@ -35,12 +35,55 @@ func blinkAtStones(stones []int64, blinks int) int {
 	return len(stones)
 }
 
+const blinksToDo int64 = 75
+
+type StoneKey struct {
+	stone  int64
+	blinks int64
+}
+
+func recursivelyCachedBlink(stone int64, blinksLeft int64, cache map[StoneKey]int64) int64 {
+	currentBlink := blinksToDo - blinksLeft + 1
+	stoneKey := StoneKey{stone: stone, blinks: currentBlink}
+
+	if cached := cache[stoneKey]; cached > 0 {
+		return cached
+	}
+	if blinksLeft == 0 {
+		return 1
+	}
+	if stone == 0 {
+		count := recursivelyCachedBlink(1, blinksLeft-1, cache)
+		cache[stoneKey] = count
+		return count
+	}
+	if length := util.IntLength(stone); length%2 == 0 {
+		halfway := util.AddZeros(1, length/2)
+		left := stone / halfway
+		right := stone % halfway
+		countL := recursivelyCachedBlink(left, blinksLeft-1, cache)
+		countR := recursivelyCachedBlink(right, blinksLeft-1, cache)
+		cache[stoneKey] = countL + countR
+		return countL + countR
+	}
+	count := recursivelyCachedBlink(stone*2024, blinksLeft-1, cache)
+	cache[stoneKey] = count
+	return count
+}
+
 func Run() {
 	file := util.OpenFileOrPanicPlz("./11/input.txt")
 	defer util.CloseFileOrPanicPlz(file)
 	scanner := util.GiveMeAScannerPlz(file)
 	input := util.ReadNumberLines(scanner)[0]
 
-	result1 := blinkAtStones(input, 25)
-	fmt.Println("11.1 - stones", result1)
+	result1 := naiveBlink(input, 25)
+	fmt.Println("11.1 - blink25", result1)
+
+	var sum int64
+	var cache map[StoneKey]int64 = make(map[StoneKey]int64)
+	for _, one := range input {
+		sum += recursivelyCachedBlink(one, blinksToDo, cache)
+	}
+	fmt.Println("11.2 - blink75", sum)
 }
